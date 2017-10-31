89cebde1bd5be60a1cb9c99ec610a7fcb3b26fd4
Bilal Yaylak <bilalyaylak@gmail.com>
2017-11-01 01:59:56 +0300

    iOS project is set and ci environment is ready

---
+++ ios/NeVA/NeVA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeVA
+//
+//  Created by Bilal Yaylak on 1.11.2017.
+//  Copyright © 2017 Bilemiyorum Altan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
