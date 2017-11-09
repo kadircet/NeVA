cd1b86e04f9cfccc7c6ad7d10371731a3b06fb5f
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2017-11-09 23:27:15 +0300

    Use system daemon instead of disown

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-PIDFILE="/var/run/neva_backend.pid"
-LOGFILE="/var/log/neva_backend.log"
-
 bazel build -c opt :neva_backend
 cp -f bazel-bin/neva_backend /var/NeVA/
 
-if [ -x $PIDFILE ];
-then
-  pid=$(cat $PIDFILE)
-  kill -0 $pid && kill -9 $pid
-fi
-/var/NeVA/neva_backend >> $LOGFILE &
-echo -n $! > $PIDFILE
-disown %1
+sudo systemctl restart neva
 
 exit 0
+
