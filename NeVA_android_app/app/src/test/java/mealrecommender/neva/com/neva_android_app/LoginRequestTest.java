adb774f68f2c92ac2c28131c68a042b7b8e5b211
Hakan Bostan <e2098812@ceng.metu.edu.tr>
2017-11-14 23:25:41 +0300

    clean-up gradle files

--- NeVA_android_app/app/src/test/java/mealrecommender/neva/com/neva_android_app/LoginRequestTest.java
+++ NeVA_android_app/app/src/test/java/mealrecommender/neva/com/neva_android_app/LoginRequestTest.java
@@ -1,8 +1,36 @@
 package mealrecommender.neva.com.neva_android_app;
 
+import org.junit.Test;
+
+import backend.BackendGrpc;
+import backend.BackendOuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by hakan on 11/14/17.
  */
 
 public class LoginRequestTest {
+
+    @Test
+    public void login_test(){
+        try {
+            ManagedChannel mChannel = ManagedChannelBuilder.forAddress("0xdeffbeef.com",50051).build();
+            System.out.println(mChannel.getState(true));
+            BackendGrpc.BackendBlockingStub blockingStub = BackendGrpc.newBlockingStub(mChannel);
+            BackendOuterClass.LoginRequest loginRequest = BackendOuterClass.LoginRequest.newBuilder().setEmail("test_email").setPassword("test_passwd").build();
+            BackendOuterClass.LoginReply loginReply = blockingStub.login(loginRequest);
+        }
+        catch (Exception e)
+        {
+            System.out.println("ERROR-Login Req.");
+            System.out.println(e);
+        }
+
+    }
 }
+
+
