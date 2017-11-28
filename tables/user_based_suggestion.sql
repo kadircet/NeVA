b8df8d18bc1051c2616f84bca0a98c97c15f6d85
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2017-11-28 15:16:14 +0300

    Define tag and tag_value proposition tables.

--- tables/user_based_suggestion.sql
+++ tables/user_based_suggestion.sql
@@ -16,3 +16,35 @@ CREATE TABLE IF NOT EXISTS `item_suggestion` (
     ON DELETE CASCADE,
   PRIMARY KEY(`id`)
 );
+
+/* Contains information on tags proposed by users.
+ * id is the unique identifier of the proposition.
+ * user_id is the id of the user who proposed it.
+ * tag is the name of the tag proposed by the user.
+ */
+CREATE TABLE IF NOT EXISTS `tag_suggestion` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user_id` INTEGER NOT NULL,
+  `tag` VARCHAR(255) NOT NULL,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE,
+  PRIMARY KEY(`id`)
+);
+
+/* Contains information on tag-item associations proposed by users.
+ * id is the unique identifier of the proposition.
+ * user_id is the id of the user who proposed it.
+ * tag_id is id of the tag that participates in relation.
+ * sugguestee_id is id of the item that particiaptes in relation.
+ * value is propsed value for tag if exists.
+ */
+CREATE TABLE IF NOT EXISTS `tag_value_suggestion` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user_id` INTEGER NOT NULL,
+  `tag_id` INTEGER NOT NULL,
+  `suggestee_id` INTEGER NOT NULL,
+  `value` VARCHAR(255),
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE,
+  FOREIGN KEY(`tag_id`) REFERENCES `tag`(`id`) ON DELETE CASCADE,
+  FOREIGN KEY(`suggestee_id`) REFERENCES `suggestee`(`id`) ON DELETE CASCADE,
+  PRIMARY KEY(`id`)
+);
