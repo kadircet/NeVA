fcb8cb4eafd37798a9c460f3ec977719541281e8
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2017-12-07 11:12:14 +0300

    Update deployment script to copy service file as well.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ bazel build -c opt :neva_backend
 cp -f bazel-bin/neva_backend /var/NeVA/
 cp -f prod.flags /var/NeVA/prod.flags
 
+sudo cp -f neva /etc/init.d/neva
+sudo systemctl daemon-reload
 sudo systemctl restart neva
 
 exit 0
