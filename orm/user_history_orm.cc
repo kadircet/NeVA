baff29dbbfe676389a064801690f7e08065a4b04
Ahmet Alperen Akcay <e2035566@ceng.metu.edu.tr>
2017-12-19 22:58:52 +0300

    Implemented Returning Choice Id Logic

--- orm/user_history_orm.cc
+++ orm/user_history_orm.cc
@@ -13,7 +13,7 @@ using grpc::StatusCode;
 }  // namespace
 
 Status UserHistoryOrm::InsertChoice(const uint32_t user_id,
-                                    const Choice& choice) {
+                                    const Choice& choice, int* choice_id) {
   if (!conn_->ping()) {
     return Status(StatusCode::UNKNOWN, "SQL server connection faded away.");
   }
@@ -22,14 +22,16 @@ Status UserHistoryOrm::InsertChoice(const uint32_t user_id,
       "INSERT INTO `user_choice_history` (`user_id`, `suggestee_id`, "
       "`timestamp`, `latitude`, `longitude`) VALUES (%0, %1, %2, %3, %4)");
   query.parse();
-  if (!query.execute(user_id, choice.suggestee_id(),
+  const mysqlpp::SimpleResult res = query.execute(user_id, choice.suggestee_id(),
                      choice.timestamp().seconds(), choice.latitude(),
-                     choice.longitude())) {
+                     choice.longitude());
+  if (!res) {
     VLOG(1) << "Something went wrong while inserting:\n"
             << choice.DebugString() << "\nInfo:" << query.info();
     return Status(StatusCode::INTERNAL, "Internal server error.");
   }
-  VLOG(1) << choice.DebugString() << " inserted succesfully.";
+  *choice_id = res.insert_id();
+  VLOG(1) << choice.DebugString() << " inserted succesfully with id: " << *choice_id;
   return Status::OK;
 }
 
