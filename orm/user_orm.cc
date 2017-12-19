2c1bd3fb4a0c99e67dbd2e7d8d26fa42eed2201a
Ahmet Alperen Akcay <e2035566@ceng.metu.edu.tr>
2017-12-19 15:09:13 +0300

    Fix user_verification and user_info insertion to database

--- orm/user_orm.cc
+++ orm/user_orm.cc
@@ -168,6 +168,7 @@ Status UserOrm::InsertUser(
       query
           << "INSERT INTO `user_verification` (`id`, `token`, `expire`) VALUES "
              "(%0, %1q, %2)";
+      query.parse();
       query.execute(user_id, util::HMac(salt, *verification_token),
                     util::GetTimestamp() + kVerficationTokenExpireTime);
       query.reset();
@@ -175,46 +176,47 @@ Status UserOrm::InsertUser(
 
     query << "INSERT INTO `user_info` (`id`, `register_date`) "
              "VALUES (%0, %1)";
+    query.parse();
     query.execute(user_id, util::GetTimestamp());
     query.reset();
 
-    if(user.has_date_of_birth())
-    {
-      query << "UPDATE `user_info` " 
+    if (user.has_date_of_birth()) {
+      query << "UPDATE `user_info` "
                "SET `date_of_birth`=%0 "
                "WHERE `id`=%1";
+      query.parse();
       query.execute(user.date_of_birth().seconds(), user_id);
       query.reset();
     }
-    if(!user.name().empty())
-    {
-      query << "UPDATE `user_info` " 
+    if (!user.name().empty()) {
+      query << "UPDATE `user_info` "
                "SET `name`=%0q "
                "WHERE `id`=%1";
+      query.parse();
       query.execute(user.name(), user_id);
       query.reset();
     }
-    if(user.gender() != 0)
-    {
-      query << "UPDATE `user_info` " 
+    if (user.gender() != 0) {
+      query << "UPDATE `user_info` "
                "SET `gender`=%0 "
                "WHERE `id`=%1";
+      query.parse();
       query.execute(user.gender(), user_id);
       query.reset();
     }
-    if(user.weight() != 0)
-    {
-      query << "UPDATE `user_info` " 
+    if (user.weight() != 0) {
+      query << "UPDATE `user_info` "
                "SET `weight`=%0 "
                "WHERE `id`=%1";
+      query.parse();
       query.execute(user.weight(), user_id);
       query.reset();
     }
-    if(!user.photo().empty())
-    {
-      query << "UPDATE `user_info` " 
+    if (!user.photo().empty()) {
+      query << "UPDATE `user_info` "
                "SET `photo`=%0q "
                "WHERE `id`=%1";
+      query.parse();
       query.execute(user.photo(), user_id);
       query.reset();
     }
--- util/time.cc
+++ util/time.cc
@@ -7,7 +7,9 @@ namespace backend {
 namespace util {
 
 uint64_t GetTimestamp() {
-  return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+  return std::chrono::duration_cast<std::chrono::seconds>(
+             std::chrono::system_clock::now().time_since_epoch())
+      .count();
 }
 
 }  // namespace util
