092609262bb2689fc783ab0aec827a3bd47c5490
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2017-12-21 17:40:01 +0300

    Update namespaces.

--- util/file.cc
+++ util/file.cc
@@ -3,6 +3,10 @@
 #include <fstream>
 #include <sstream>
 
+namespace neva {
+namespace backend {
+namespace util {
+
 void ReadFile(const std::string& file_name, std::string* buffer) {
   std::ifstream file(file_name.c_str(), std::ios::in);
   if (file.is_open()) {
@@ -12,3 +16,6 @@ void ReadFile(const std::string& file_name, std::string* buffer) {
     *buffer = stream.str();
   }
 }
+}  // namespace util
+}  // namespace backend
+}  // namespace neva
