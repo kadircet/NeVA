7f15d2a01c8aea479122e59c9793a4a1f18be622
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2017-12-24 14:27:50 +0300

    Add staging logic to gitlab-ci and add new service.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-bazel build -c opt :neva_backend
-cp -f bazel-bin/neva_backend /var/NeVA/
-cp -f prod.flags /var/NeVA/prod.flags
+BINARY_NAME="neva_backend_staging"
+FLAGFILE_NAME="stag.flags"
+APK_NAME="android_staging.apk"
+SERVICE_NAME="neva_staging"
 
-sudo cp -f neva /etc/init.d/neva
+if [ "production" == $1 ];
+  BINARY_NAME="neva_backend"
+  FLAGFILE_NAME="prod.flags"
+  APK_NAME="android.apk"
+  SERVICE_NAME="neva"
+fi
+
+bazel build -c opt :neva_backend
+cp -f bazel-bin/neva_backend /var/NeVA/$BINARY_NAME
+cp -f flags.file /var/NeVA/$FLAGFILE_NAME
+
+sudo cp -f $SERVICE_NAME /etc/init.d/$SERVICE_NAME
 sudo systemctl daemon-reload
-sudo systemctl restart neva
+sudo systemctl restart $SERVICE_NAME
 
 sudo cp -f /tmp/neva_android/outputs/apk/debug/app-debug.apk \
-  /var/www/neva/android.apk
+  /var/www/neva/$APK_NAME
 
 exit 0
 
