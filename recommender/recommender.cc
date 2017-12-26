3ff465cf889c8748e55e0a13632a40768a09e2a1
Ahmet Alperen Akcay <e2035566@ceng.metu.edu.tr>
2017-12-27 00:35:32 +0300

    Fixed a bug about proto usage

--- recommender/recommender.cc
+++ recommender/recommender.cc
@@ -54,12 +54,12 @@ SuggestionList GetSuggestion(const UserHistory& history,
   }
 
   // Return all most frequent elements
-  SuggestionList suggested_list;
+  SuggestionList* suggested_list;
   for (const auto& it : max_freq_ids) {
-    suggested_list.add_suggestion_list() = *id_to_suggestee[it];
+    suggested_list->add_suggestion_list() = *id_to_suggestee[it];
   }
 
-  return suggested_list;
+  return *suggested_list;
 }
 
 }  // namespace recommender
