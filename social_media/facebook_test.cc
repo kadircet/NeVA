c672f20a48c3a586cdc3395967432668f7e09cb1
HAKAN BOSTAN <e2098812@ceng.metu.edu.tr>
2018-02-19 21:35:43 +0300

    Update facebook_test.cc

--- social_media/facebook_test.cc
+++ social_media/facebook_test.cc
@@ -9,7 +9,7 @@ namespace {
 constexpr const char* const kEmail = "cbogrqqktr_1511716461@tfbnw.net";
 constexpr const char* const kAuthTokenInvalid = "asdfasdfqwerasdf";
 constexpr const char* const kAuthTokenValid =
-    R"(EAACZAu8B33nYBAHyARqh8V6tQEF84eRMGC5UNwXEJM79aicwfFcnZCfSZBugZAZBxZCcGZCpHEZBQIlCJinFfzoSjUpY8EvZAtWcHwpQBxftzUDxQO2VKehD5mV83lNwSiAKr0uYEBGYkiZC7cJvFql0K2gGdTlh8atpUTzeUotEhbjVgyYeYCwpWZA)";
+    R"(EAACZAu8B33nYBAGFp8blpFnU8l7WrJkw5HsogZAZBNwB03QVrJ3Hxr7QwUdZBQiSfT6sajdWYniZBsSbD21qTKkReubAZCsEfwdxXEZAIWzLqYjKlH7kPmuZBNCrjF85FoOQIXNSWVRUnT4fQ7ZB1Oj1SXhmKbVoh354K3GDCskslO4BguaJU2YoHvzMwUQ9BHmee6FfrRptqWJgyJ1h87ZBceVZBENPe9CAJ2wK66pZBN6pjgZDZD)";
 
 TEST(Validate, Valid) { EXPECT_TRUE(Validate(kEmail, kAuthTokenValid)); }
 
