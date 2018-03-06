d2e32e306dfbb21abe792bd3e5edcdc7ce1561d8
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-03-06 23:33:12 +0300

    Add table definitions for holding suggestion info.

---
+++ tables/recommender.sql
@@ -0,0 +1,10 @@
+USE `neva`;
+
+CREATE TABLE IF NOT EXISTS `recommender_cache` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user_id` INTEGER NOT NULL,
+  `suggestee_id` INTEGER NOT NULL,
+  FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE,
+  FOREIGN KEY(`suggestee_id`) REFERENCES `suggestee`(`id`) ON DELETE CASCADE,
+  PRIMARY KEY(`id`)
+);
