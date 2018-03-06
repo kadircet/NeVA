8a43fd0195fbbdf0e49ce5481a3fb8b4e4eb8656
Ahmet Alperen Akcay <e2035566@ceng.metu.edu.tr>
2018-03-07 01:47:48 +0300

    Compile fix

--- orm/user_orm.cc
+++ orm/user_orm.cc
@@ -119,7 +119,7 @@ Status UserOrm::CheckCredentials(
   do {
     *session_token = util::GenerateRandomKeySecure();
     res = query.store(*session_token);
-  } while(!res.empty())
+  } while(!res.empty());
 
   // TODO(kadircet): Implement token expiration.
   query << "INSERT INTO `user_session` (`user_id`, `token`, `expire`) VALUES "
