fec9470ef8ee8feb5f84c74510d95c15a787a39b
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-03-16 10:46:00 +0300

    Add deployment commands for recommender files and service.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+RECOMMENDER_SERVICE_NAME="neva_recommender"
 BINARY_NAME="neva_backend_staging"
 FLAGFILE_NAME="stage.flags"
 APK_NAME="android_staging.apk"
@@ -14,10 +15,14 @@ fi
 bazel build -c opt :neva_backend
 cp -f bazel-bin/neva_backend /var/NeVA/$BINARY_NAME
 cp -f flags.file /var/NeVA/$FLAGFILE_NAME
+cp -r recommender/*.py /var/NeVA/recommender/
 
 sudo cp -f $SERVICE_NAME /etc/init.d/$SERVICE_NAME
+sudo cp -f $RECOMMENDER_SERVICE_NAME /etc/init.d/$RECOMMENDER_SERVICE_NAME
+
 sudo systemctl daemon-reload
 sudo systemctl restart $SERVICE_NAME
+sudo systemctl restart $RECOMMENDER_SERVICE_NAME
 
 sudo cp -f /tmp/neva_android/outputs/apk/debug/app-debug.apk \
   /var/www/neva/$APK_NAME
