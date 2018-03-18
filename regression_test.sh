191f015c9d7ac973b78ca7a08812ac4d14efd01a
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-03-18 13:04:47 +0300

    Update regression test format.

--- regression_test.sh
+++ regression_test.sh
@@ -1,6 +1,16 @@
 GRPC_CLI=/home/kadircet/repos/grpc/bazel-bin/test/cpp/util/grpc_cli
 ARGS="--proto_path=protos/ --enable_ssl call neva.0xdeffbeef.com:50051"
-TOKEN=$($GRPC_CLI $ARGS Login 'email:"hkn@test.com" password:"asdfASDF" authentication_type: DEFAULT' 2> /dev/null | grep -i "token")
+
+LOGIN_EMAIL='email: "hkn@test.com"'
+LOGIN_PASSWORD='password: "asdfASDF"'
+AUTH_TYPE="authentication_type: DEFAULT"
+
+LOGIN_EMAIL='email: "10156039002563534"'
+LOGIN_PASSWORD='password: "EAACZAu8B33nYBAL5YYjoDWqRCXoCpbeyEayoEU6xiUqfX289YqMkdns9p73ZAsoSFouS2jOZAoSGBrXNi2dqJddYaZAN9ymiYEM2bEZCZCB7pZAJyvOIYN9X1ZACKul8zNhrYZAaPSfkdZBBAE8jGMRZBaakNVUxttGg6sZD"'
+AUTH_TYPE="authentication_type: FACEBOOK"
+
+TOKEN=$(echo $LOGIN_EMAIL $LOGIN_PASSWORD $AUTH_TYPE | $GRPC_CLI $ARGS "Login" \
+  2> /dev/null | grep -i "token")
 
 for i in $(seq 0 100)
 do
