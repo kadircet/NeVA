940b17633e2cd629e142f03da29dda2072aa86ce
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-03-19 16:07:38 +0300

    Update database username.

--- moderation/neva_moderation.py
+++ moderation/neva_moderation.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_USER'] = 'neva'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'neva'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
