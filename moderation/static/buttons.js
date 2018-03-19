618f43c5dda95ae7ca751d640c576acd39b412c6
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-03-19 15:53:48 +0300

    Code formatting.

--- moderation/static/buttons.js
+++ moderation/static/buttons.js
@@ -1,80 +1,80 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   $('.acceptMeal').on('click', function() {
     var table_id = $(this).attr('table_id');
-    var meal_name = $('#meal_name'+table_id).val();
-    var action = "acceptMeal";
+    var meal_name = $('#meal_name' + table_id).val();
+    var action = 'acceptMeal';
     req = sendPostReq(table_id, meal_name, action);
-    req.done(function(data){
-      if(data.result=='success'){
-        $('#meal_row_id'+table_id).fadeOut(200);
+    req.done(function(data) {
+      if (data.result == 'success') {
+        $('#meal_row_id' + table_id).fadeOut(200);
       }
     })
   });
 
   $('.rejectMeal').on('click', function() {
     var table_id = $(this).attr('table_id');
-    var meal_name = $('#meal_name'+table_id).val();
-    var action = "rejectMeal";
-    req =  sendPostReq(table_id, meal_name, action);
-    req.done(function(data){
-      if(data.result=='success'){
-        $('#meal_row_id'+table_id).fadeOut(200);
+    var meal_name = $('#meal_name' + table_id).val();
+    var action = 'rejectMeal';
+    req = sendPostReq(table_id, meal_name, action);
+    req.done(function(data) {
+      if (data.result == 'success') {
+        $('#meal_row_id' + table_id).fadeOut(200);
       }
     })
   });
 
   $('.acceptTag').on('click', function() {
     var table_id = $(this).attr('table_id');
-    var tag_name = $('#tag_name'+table_id).val();
-    var action = "acceptTag";
+    var tag_name = $('#tag_name' + table_id).val();
+    var action = 'acceptTag';
 
     req = sendPostReq(table_id, tag_name, action);
-    req.done(function(data){
-      if(data.result=='success'){
-        $('#tag_row_id'+table_id).fadeOut(200);
+    req.done(function(data) {
+      if (data.result == 'success') {
+        $('#tag_row_id' + table_id).fadeOut(200);
       }
     })
-    
+
   });
 
   $('.rejectTag').on('click', function() {
     var table_id = $(this).attr('table_id');
-    var tag_name = $('#tag_name'+table_id).val();
-    var action = "rejectTag";
+    var tag_name = $('#tag_name' + table_id).val();
+    var action = 'rejectTag';
     req = sendPostReq(table_id, tag_name, action);
-    req.done(function(data){
-      if(data.result=='success'){
-        $('#tag_row_id'+table_id).fadeOut(200);
+    req.done(function(data) {
+      if (data.result == 'success') {
+        $('#tag_row_id' + table_id).fadeOut(200);
       }
     })
   });
 
   $('.acceptTagValue').on('click', function() {
     var table_id = $(this).attr('table_id');
-    var meal_name = $('#tag_value_suggestee_name'+table_id).val();
-    var meal_id = $('#tag_value_suggestee_name'+table_id).attr("db_id");
+    var meal_name = $('#tag_value_suggestee_name' + table_id).val();
+    var meal_id = $('#tag_value_suggestee_name' + table_id).attr('db_id');
 
-    var tag_name = $('#tag_value_tag_name'+table_id).val();
-    var tag_id = $('#tag_value_tag_name'+table_id).attr("db_id");
-    var action = "acceptTagValue";
+    var tag_name = $('#tag_value_tag_name' + table_id).val();
+    var tag_id = $('#tag_value_tag_name' + table_id).attr('db_id');
+    var action = 'acceptTagValue';
     req = sendPostReqTagValue(table_id, meal_id, tag_id, action);
-    req.done(function(data){
-      if(data.result=='success'){
-        $('#tag_value_row_id'+table_id).fadeOut(200);
+    req.done(function(data) {
+      if (data.result == 'success') {
+        $('#tag_value_row_id' + table_id).fadeOut(200);
       }
     })
   });
 
   $('.rejectTagValue').on('click', function() {
     var table_id = $(this).attr('table_id');
-    var meal_id = $('#tag_value_suggestee_name'+table_id).attr("db_id");
-    var tag_id = $('#tag_value_tag_name'+table_id).attr("db_id");
-    var action = "rejectTagValue";
+    var meal_id = $('#tag_value_suggestee_name' + table_id).attr('db_id');
+    var tag_id = $('#tag_value_tag_name' + table_id).attr('db_id');
+    var action = 'rejectTagValue';
     req = sendPostReqTagValue(table_id, meal_id, tag_id, action);
-    req.done(function(data){
-      if(data.result=='success'){
-        $('#tag_value_row_id'+table_id).fadeOut(200);
+    req.done(function(data) {
+      if (data.result == 'success') {
+        $('#tag_value_row_id' + table_id).fadeOut(200);
       }
     })
   });
@@ -85,18 +85,18 @@ $(document).ready(function(){
 
 function sendPostReq(table_id, name, act) {
   req = $.ajax({
-    url : '/process',
+    url: '/process',
     type: 'POST',
-    data: { id : table_id, suggestion: name, action : act}
+    data: {id: table_id, suggestion: name, action: act}
   });
   return req;
 }
 
 function sendPostReqTagValue(table_id, meal_id, tag_id, action) {
   req = $.ajax({
-    url : '/processTagValue',
+    url: '/processTagValue',
     type: 'POST',
-    data: { id:table_id, meal_id:meal_id, tag_id:tag_id, action:action}
+    data: {id: table_id, meal_id: meal_id, tag_id: tag_id, action: action}
   });
   return req;
 }
