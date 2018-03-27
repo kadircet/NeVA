c55b7c330d3ae4ffbeaf21225c07f45b9ead9965
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-03-28 00:36:04 +0300

    Update clustering to fetch and send suggestee ids.

--- recommender/clustering.py
+++ recommender/clustering.py
@@ -7,6 +7,7 @@ import numpy as np
 # TODO(kadircet): Implement support for event based updating after a user's
 # like/dislike/history information arrives.
 def main():
+    suggestees = utils.GetSuggesteeIDs()
     _cacher = cacher.Cacher()
     now = datetime.datetime.now()
     time = now.hour * 60 + now.minute
@@ -15,7 +16,7 @@ def main():
     # TODO(kadircet): Perform loop in parallel.
     for user_id in utils.GetUserIDs():
         nearest_elements = utils.GetNearestElements(
-            user_id, current_context, k=10)
+            user_id, current_context, suggestees, k=10)
         _cacher.UpdateUserCache(user_id, nearest_elements)
 
 
