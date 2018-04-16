146339220fa28fcdcc6e3746a6e161a733d8a6a1
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-04-16 21:52:32 +0300

    Define coverage binary.

---
+++ util/kcov.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+me=$(python -c 'import os; print(os.path.realpath("'"$0"'"))')
+WORKSPACE=$(dirname $(dirname "$me"))
+
+if [ ! -f "${WORKSPACE}/WORKSPACE" ]; then
+  echo "File not found: ${WORKSPACE}/WORKSPACE"
+  exit 1
+fi
+
+kcov \
+    --include-path=$WORKSPACE \
+    --verify \
+    --exclude-pattern=external,test \
+    $WORKSPACE/bazel-kcov \
+    --replace-src-path=/proc/self/cwd:$WORKSPACE \
+    "$@"
