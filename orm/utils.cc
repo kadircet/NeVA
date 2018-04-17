6ff3487fd35828b948ba82e03ed0492f581fd725
Bilal Yaylak <e2031581@ceng.metu.edu.tr>
2018-04-18 00:15:30 +0300

    Fixed compilation error

---
+++ orm/utils.cc
@@ -0,0 +1,26 @@
+#include "orm/utils.h"
+
+namespace neva {
+namespace backend {
+namespace orm {
+namespace {
+  
+  using grpc::Status;
+  using grpc::StatusCode;
+  
+} // namespace
+  
+  void GetTags(const mysqlpp::ScopedConnection& conn, Suggestion* suggestee) {
+    mysqlpp::Query query = conn->query(
+                                       "SELECT `tag_id` FROM `suggestee_tags` WHERE `suggestee_id`=%0");
+    query.parse();
+    const mysqlpp::StoreQueryResult res = query.store(suggestee->suggestee_id());
+    for (const auto row : res) {
+      Tag* tag = suggestee->add_tags();
+      tag->set_id(row["tag_id"]);
+    }
+  }
+  
+}  // namespace orm
+}  // namespace backend
+}  // namespace neva
--- orm/utils.h
+++ orm/utils.h
@@ -15,16 +15,7 @@ namespace {
   
   // Fetches tags associated with given suggestee and stores them into it.
   // Assumes suggestee->suggestee_id is set.
-  void GetTags(const mysqlpp::ScopedConnection& conn, Suggestion* suggestee) {
-    mysqlpp::Query query = conn->query(
-                                       "SELECT `tag_id` FROM `suggestee_tags` WHERE `suggestee_id`=%0");
-    query.parse();
-    const mysqlpp::StoreQueryResult res = query.store(suggestee->suggestee_id());
-    for (const auto row : res) {
-      Tag* tag = suggestee->add_tags();
-      tag->set_id(row["tag_id"]);
-    }
-  }
+  void GetTags(const mysqlpp::ScopedConnection& conn, Suggestion* suggestee);
   
 }  // namespace orm
 }  // namespace backend
