b96a8859e642a6578987b9ba72c89382ba848e42
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-04-17 14:09:04 +0300

    Enable logging at recommender startup.

--- recommender/driver.py
+++ recommender/driver.py
@@ -1,4 +1,7 @@
 import sys
+import os
+import datetime
+import logging
 import traceback
 import time
 kWaitInteval = 1.  # 1 second.
@@ -11,9 +14,17 @@ def main():
         try:
             clustering.main()
         except Exception as e:
-            print(e)
+            logging.log(logging.WARN, e, exc_info=1)
         time.sleep(kWaitInteval)
 
 
 if __name__ == "__main__":
+    now = str(datetime.datetime.now()).replace(" ", "_")
+    log_file_name = os.path.join(
+        os.path.dirname(sys.argv[0]),
+        'logs/' + now + "_" + str(os.getpid()) + '.log')
+    logging.basicConfig(
+        filename=log_file_name,
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(name)s %(message)s')
     main()
