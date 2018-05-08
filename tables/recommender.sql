8aee5a45467f48cc230013579dd2be57a28d6171
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-05-08 19:03:47 +0300

    Define diverse item cache table.

--- tables/recommender.sql
+++ tables/recommender.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS `recommender_cache` (
   FOREIGN KEY(`suggestee_id`) REFERENCES `suggestee`(`id`) ON DELETE CASCADE,
   PRIMARY KEY(`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `diverse_item_cache` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `suggestee_id` INTEGER NOT NULL,
+  FOREIGN KEY(`suggestee_id`) REFERENCES `suggestee`(`id`) ON DELETE CASCADE,
+  PRIMARY KEY(`id`)
+);
