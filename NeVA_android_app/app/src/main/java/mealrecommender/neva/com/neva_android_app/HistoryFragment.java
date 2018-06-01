b9ca3b44372c3486f6c592c8fad059d7af731b33
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-06-01 20:46:35 +0300

    import ListViewCompat.

--- NeVA_android_app/app/src/main/java/mealrecommender/neva/com/neva_android_app/HistoryFragment.java
+++ NeVA_android_app/app/src/main/java/mealrecommender/neva/com/neva_android_app/HistoryFragment.java
@@ -3,6 +3,7 @@ package mealrecommender.neva.com.neva_android_app;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v7.widget.ListViewCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
