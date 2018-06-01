8e8beadca1d378ab0bda378fd94e94078b6e9c87
kadir cetinkaya <e2036457@ceng.metu.edu.tr>
2018-06-01 21:05:08 +0300

    Fix build.

--- NeVA_android_app/app/src/main/java/mealrecommender/neva/com/neva_android_app/HistoryFragment.java
+++ NeVA_android_app/app/src/main/java/mealrecommender/neva/com/neva_android_app/HistoryFragment.java
@@ -3,7 +3,6 @@ package mealrecommender.neva.com.neva_android_app;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.support.v7.widget.ListViewCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
